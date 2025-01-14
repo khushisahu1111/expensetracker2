7f0223723b7174e295694d0d2408284293b445b1
khushisahu <sahuk7069@gmail.com>
2025-01-14 16:46:47 +0530

    set up the react and node js

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const  express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+
+    res.send("i am khushi");
+})
+app.listen(6969,()=>{
+    console.log("server is running on port 6969");
+})
+const mongodb=require('mongodb');
+
+
