f1c4d0c6f35b1b923742df1fe104321ae4d6ffe3
lavanya2573 <sakshi25gautam@gmail.com>
2025-01-16 10:21:40 +0530

    i change my name

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const  express=require('express');
 const app=express();
 app.get('/',(req,res)=>{
 
-    res.send("i am khushi");
+    res.send("i am sakshi");
 })
 app.listen(6969,()=>{
     console.log("server is running on port 6969");
